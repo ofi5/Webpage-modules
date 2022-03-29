1bd8c8ed6c2dd8ac97cb3abd0583c9a4ea9c3bb5
ofi5 <86188587+ofi5@users.noreply.github.com>
2022-03-29 23:07:13 +0530

    update

--- CSS practice/module 8/index.js
+++ CSS practice/module 8/index.js
@@ -1,17 +1,57 @@
-firstCard = 10
-secondCard = 11
-sum = firstCard + secondCard
 
+let sum = 0
+let firstCard
+let secondCard
+let card
+let cards = []
+
+
+let messageEl = document.getElementById("message-el")
+
+let cardsEl = document.getElementById("cards-el")
+let sumEl = document.getElementById("sum-el")
+
+
+function newCard() {
+        card = getRandomCard()
+        cards.push(card)
+}
 
 function renderGame() {
+        cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+        sumEl.textContent = "Sum: " + sum
+
     
+
+
      if (sum > 21) {
-        console.log("You are out")
+        messageEl.textContent ="You are out"
 }    else if (sum === 21) {
-        console.log("blackjack")
+        messageEl.textContent = "blackjack"
 }    else {
-        console.log("Draw a new Card?")
+        messageEl.textContent = "Draw a new Card?"
 }
 }
 
 
+function startGame() {
+        cards = []
+        firstCard = getRandomCard()
+        secondCard = getRandomCard()
+        cards.push(firstCard)
+        cards.push(secondCard)
+        sum = firstCard + secondCard
+        renderGame()
+        console.log(cards)
+
+
+}
+
+function getRandomCard() {
+
+        let randomNumber = Math.floor(Math.random()*13) + 1
+
+        return randomNumber
+}
+
+console.log(cards)
