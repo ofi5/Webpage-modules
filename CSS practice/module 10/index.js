1532c3557495a69b7ea35ca8617fe59914f3816c
ofi5 <86188587+ofi5@users.noreply.github.com>
2022-04-08 01:42:01 +0530

    update

--- CSS practice/module 10/index.js
+++ CSS practice/module 10/index.js
@@ -5,6 +5,17 @@ let ulEl = document.getElementById("ul-el")
 
 btnEl.addEventListener("click", function() {
     myLeads.push(inputEl.value)
-    console.log(myLeads)
+    inputEl.value = ""
+    renderLeads()    
 
 })
+
+function renderLeads() {
+
+    listItems = ""
+    for (i = 0; i < myLeads.length; i++) {        
+        listItems += `<li><a href=${myLeads[i]} target='_blank'> ${myLeads[i]} </a></li>`
+}
+
+ulEl.innerHTML = listItems
+}
