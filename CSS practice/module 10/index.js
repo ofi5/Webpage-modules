74285bba5b0028a48eb558f47217b1a789e69943
ofi5 <86188587+ofi5@users.noreply.github.com>
2022-04-07 22:32:45 +0530

    update

--- CSS practice/module 10/index.js
+++ CSS practice/module 10/index.js
@@ -1,3 +1,10 @@
-function saveBtn() {
-    console.log("Button clicked");
-}
+myLeads = []
+let inputEl = document.getElementById("input-el")
+let btnEl = document.getElementById("btn") 
+let ulEl = document.getElementById("ul-el")
+
+btnEl.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    console.log(myLeads)
+
+})
