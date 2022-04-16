de1bc2af1b0f5c36b25de6618c6d86fbbb37ab0c
ofi5 <86188587+ofi5@users.noreply.github.com>
2022-04-17 05:17:51 +0530

    update

--- CSS practice/module 11 food website/index.js
+++ CSS practice/module 11 food website/index.js
@@ -1,6 +1,33 @@
-cartEl = document.getElementById("cart");
 
 
-function addToCart() {
+let totalEl = document.getElementById("total");
+
+let addItemId = 0;
+function addToCart(item) {
+    addItemId += 1;
     
-}
+
+    let selectedItem = document.createElement("div");
+    selectedItem.classList.add("cartImg");
+    selectedItem.setAttribute("id", addItemId);
+
+    let img = document.createElement("img");
+    img.setAttribute("src", item.children[1].currentSrc);
+
+    let pizzaName = document.createElement("span");
+    pizzaName.textContent = item.children[2].textContent;
+
+    let price = document.createElement("span");
+    price.innerText = item.children[3].textContent;
+
+    let cartEl = document.getElementById("cart");
+
+  
+    selectedItem.append(img);
+    selectedItem.append(pizzaName);
+    selectedItem.append(price);
+    cartEl.append(selectedItem);
+
+
+   totalEl.textContent = Number(price.innerText)
+} 
