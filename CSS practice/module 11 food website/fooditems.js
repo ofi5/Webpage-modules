eb4f1a0071b66e236e55f44f79f39adae14cf68b
ofi5 <86188587+ofi5@users.noreply.github.com>
2022-04-21 22:19:41 +0530

    update

--- CSS practice/module 11 food website/fooditems.js
+++ CSS practice/module 11 food website/fooditems.js
@@ -11,12 +11,158 @@ const foodItems = [
 
 
     {
-        id:1 ,
-        name:1 ,
-        img: 1,
-        price: 1,
-        category:1 ,
-        rating:1 ,
+        id:2 ,
+        name:"Deluxe Veggie" ,
+        img: "images/Deluxe_Veggie.jpg",
+        price: 250,
+        category:"Veg" ,
+        rating:4.3 ,
+        quantity:1
+    }
+,
+    {
+        id:3 ,
+        name:"Farm House" ,
+        img: "images/pizza2.PNG",
+        price: 250,
+        category:"Veg" ,
+        rating:4.3 ,
+        quantity:1
+    },{
+        id:4 ,
+        name:"Paneer Tikka" ,
+        img: "images/pizza3.PNG",
+        price: 250,
+        category:"Veg" ,
+        rating:4.3 ,
+        quantity:1
+    },{
+        id:5 ,
+        name:"Veggie Paradise" ,
+        img: "images/Digital_Veggie_Paradise_olo_266x265.jpg",
+        price: 250,
+        category:"Veg" ,
+        rating:4.3 ,
+        quantity:1
+    },{
+        id:6 ,
+        name:"Veg Extravaganza" ,
+        img: "images/Veg_Extravaganz.jpg",
+        price: 250,
+        category:"Veg",
+        rating:4.3 ,
+        quantity:1
+    },{
+        id:7 ,
+        name:"Mexican Green Wave" ,
+        img: "images/Mexican_Green_Wave.jpg",
+        price: 250,
+        category:"Veg" ,
+        rating:4.3 ,
+        quantity:1
+    },{
+        id:8 ,
+        name:"Paneer Makhni" ,
+        img: "images/Paneer_Makhni.jpg",
+        price: 250,
+        category:"Veg" ,
+        rating:4.3 ,
+        quantity:1
+    },{
+        id:9 ,
+        name:"Golden Delight" ,
+        img: "images/Chicken_Golden_Delight.jpg",
+        price: 250,
+        category:"NonVeg" ,
+        rating:4.3 ,
+        quantity:1
+    },{
+        id:10 ,
+        name:"Chicken Sausage" ,
+        img: "images/Chicken_Sausage.jpg",
+        price: 250,
+        category:"NonVeg" ,
+        rating:4.3 ,
+        quantity:1
+    },{
+        id:11 ,
+        name:"Chunky Chicken" ,
+        img: "images/chunky-chicken.png",
+        price: 250,
+        category:"NonVeg" ,
+        rating:4.3 ,
+        quantity:1
+    },{
+        id:12 ,
+        name:"Chicken Dominator" ,
+        img: "images/Dominator.jpg",
+        price: 250,
+        category:"NonVeg" ,
+        rating:4.3 ,
+        quantity:1
+    },{
+        id:13 ,
+        name:"Chicken Tikka" ,
+        img: "images/IndianChickenTikka.jpg",
+        price: 250,
+        category:"NonVeg" ,
+        rating:4.3 ,
+        quantity:1
+    },{
+        id:14 ,
+        name:"Non Veg Supreme" ,
+        img: "images/Non-Veg_Supreme.jpg",
+        price: 250,
+        category:"NonVeg" ,
+        rating:4.3 ,
+        quantity:1
+    },{
+        id:15 ,
+        name:"Pepper Barbeque" ,
+        img: "images/Pepper_Barbeque.jpg",
+        price: 250,
+        category:"NonVeg" ,
+        rating:4.3 ,
+        quantity:1
+    },{
+        id:16 ,
+        name:"Barbeque and Onion" ,
+        img: "images/Pepper_Barbeque_&_Onion.jpg",
+        price: 250,
+        category:"NonVeg" ,
+        rating:4.3 ,
+        quantity:1
+    },{
+        id:17 ,
+        name:"Cake" ,
+        img: "images/images.jpg",
+        price: 250,
+        category:"Sides" ,
+        rating:4.3 ,
+        quantity:1
+    },{
+        id:18 ,
+        name:"Garlic Bread" ,
+        img: "images/stuffed-garlic-bread-featured.jpg",
+        price: 250,
+        category:"Sides" ,
+        rating:4.3 ,
+        quantity:1
+    },{
+        id:19 ,
+        name:"Cream Mayonnaise" ,
+        img: "images/_thumb_17450.png",
+        price: 250,
+        category:"Sides" ,
+        rating:4.3 ,
+        quantity:1
+    },{
+        id:20 ,
+        name:"Tandoori Mayonnaise" ,
+        img: "images/_thumb_17451.png",
+        price: 250,
+        category:"Sides" ,
+        rating:4.3 ,
         quantity:1
     }
 ]
--- CSS practice/module 11 food website/index2.js
+++ CSS practice/module 11 food website/index2.js
@@ -1,11 +1,15 @@
 import {foodItems} from "./fooditems.js";
 
-// console.log(foodItems);
+console.log(foodItems);
 
 function displayItems(){
     let veg = document.getElementById("veg");
+    let NonVeg = document.getElementById("nonveg");
+    let sides = document.getElementById("sides");
 
     const vegData = foodItems.filter(item=> item.category=='Veg');
+    const nonVegData = foodItems.filter(item => item.category == "NonVeg");
+    const sidesData = foodItems.filter(item => item.category=="Sides");
 
     console.log(vegData);
 
@@ -45,7 +49,77 @@ function displayItems(){
         
     })
 
+    nonVegData.map(item => {
+        let menuBox = document.createElement("div");
+        menuBox.setAttribute("class", "menu-box");
+
+        let rating = document.createElement("p");
+        rating.setAttribute("class", "rating");
+        rating.textContent = '⭐' + item.rating;
+
+        let img = document.createElement("img");
+        img.setAttribute("class", "menu-img");
+        img.src = item.img;
+
+        let namePizza = document.createElement("p");
+        namePizza.setAttribute("class", "name-pizza");
+        namePizza.textContent = item.name;
+
+        let price = document.createElement("price");
+        price.setAttribute("class","price-text");
+        price.textContent = item.price;
+
+        let btn = document.createElement("button");
+        btn.setAttribute("class","cart-btn");
+        btn.innerHTML="Add to Cart";
+
+        menuBox.append(rating);
+        menuBox.append(img);
+        menuBox.append(namePizza);
+        menuBox.append(price);
+        menuBox.append(btn);
+
+        NonVeg.append(menuBox);
+
+    })
+
+    sidesData.map(item => {
+        let menuBox = document.createElement("div");
+        menuBox.setAttribute("class", "menu-box");
+
+        let rating = document.createElement("p");
+        rating.setAttribute("class", "rating");
+        rating.textContent = '⭐' + item.rating;
+
+        let img = document.createElement("img");
+        img.setAttribute("class", "menu-img");
+        img.src = item.img;
+
+        let namePizza = document.createElement("p");
+        namePizza.setAttribute("class", "name-pizza");
+        namePizza.textContent = item.name;
+
+        let price = document.createElement("price");
+        price.setAttribute("class","price-text");
+        price.textContent = item.price;
+
+        let btn = document.createElement("button");
+        btn.setAttribute("class","cart-btn");
+        btn.innerHTML="Add to Cart";
+
+        menuBox.append(rating);
+        menuBox.append(img);
+        menuBox.append(namePizza);
+        menuBox.append(price);
+        menuBox.append(btn);
+
+        sides.append(menuBox);
+
+    })
+
 }
 
 displayItems();
 
+
+    
