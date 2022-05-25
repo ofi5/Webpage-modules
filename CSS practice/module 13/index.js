ecf510a37853341b9b165b8a8fe878a770e51a82
ofi5 <86188587+ofi5@users.noreply.github.com>
2022-05-25 23:48:01 +0530

    update

--- CSS practice/module 13/index.js
+++ CSS practice/module 13/index.js
@@ -1,4 +1,25 @@
 let start = document.querySelector(".start");
 let gameArea = document.querySelector(".gameArea");
 let score = document.querySelector("#score");
-start.innerHTML= "Do you want to start game ?"
+
+
+start.addEventListener("click", startGame);
+
+function startGame() {
+    console.log('game started');
+}
+
+
+document.addEventListener("keydown", keyOn);
+document.addEventListener("keyup",keyOff);
+
+
+function keyOn(event) {
+    event.preventDefault();
+    console.log(event.key);
+}
+
+function keyOff(event) {
+    event.preventDefault();
+    console.log(event.key);
+}
