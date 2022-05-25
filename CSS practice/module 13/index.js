e5130846744a9b533d1637a754ddb204b9b2a9f2
ofi5 <86188587+ofi5@users.noreply.github.com>
2022-05-26 02:31:41 +0530

    update

--- CSS practice/module 13/index.js
+++ CSS practice/module 13/index.js
@@ -1,3 +1,4 @@
+
 let start = document.querySelector(".start");
 let gameArea = document.querySelector(".gameArea");
 let score = document.querySelector("#score");
@@ -6,7 +7,7 @@ let score = document.querySelector("#score");
 start.addEventListener("click", startGame);
 
 
-let player = {};
+let player = {speed:5};
 
 function startGame() {
 
@@ -18,15 +19,30 @@ function startGame() {
     console.log(player);
     window.requestAnimationFrame(playGame);
     let car = document.createElement("div");
+    car.classList.add("class","car");
     car.innerHTML = "Car go Zoooom !!"
+    console.log(player);
     gameArea.append(car);
+    player.x = car.offsetLeft;
+    player.y = car.offsetTop;
+    console.log(player);
 }
 
 function playGame() {
     console.log("inplay");
+    let userCar = document.querySelector(".car");
+
     if(player.ready){
+        if(keys.ArrowUp) {player.y +=player.speed};
+        if(keys.ArrowDown) {player.y -=player.speed};
+        if(keys.ArrowLeft) {player.x -=player.speed};
+        if(keys.ArrowRight) {player.x +=player.speed};
+
+        userCar.style.bottom= player.y + "px";
+        userCar.style.left = player.x + "px";
     window.requestAnimationFrame(playGame);
-}}
+}
+}
 
 document.addEventListener("keydown", keyOn);
 document.addEventListener("keyup",keyOff);
