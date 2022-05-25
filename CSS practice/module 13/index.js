fd72d0f3f18cb906b37178aa91fbf3bad0a0806f
ofi5 <86188587+ofi5@users.noreply.github.com>
2022-05-25 23:11:29 +0530

    update

--- CSS practice/module 13/index.js
+++ CSS practice/module 13/index.js
@@ -1,3 +1,4 @@
-let start = document.querySelector(".start")
-
+let start = document.querySelector(".start");
+let gameArea = document.querySelector(".gameArea");
+let score = document.querySelector("#score");
 start.innerHTML= "Do you want to start game ?"
