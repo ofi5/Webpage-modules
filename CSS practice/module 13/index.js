66c94c51a1f7fbab49c757cabb5615e2af367a26
ofi5 <86188587+ofi5@users.noreply.github.com>
2022-05-26 01:21:22 +0530

    Update index.js

--- CSS practice/module 13/index.js
+++ CSS practice/module 13/index.js
@@ -5,21 +5,46 @@ let score = document.querySelector("#score");
 
 start.addEventListener("click", startGame);
 
+
+let player = {};
+
 function startGame() {
+
+    start.classList.toggle("hide");
+    gameArea.classList.toggle("hide");
+
     console.log('game started');
+    player.ready = true;
+    console.log(player);
+    window.requestAnimationFrame(playGame);
+    let car = document.createElement("div");
+    car.innerHTML = "Car go Zoooom !!"
+    gameArea.append(car);
 }
 
+function playGame() {
+    console.log("inplay");
+    if(player.ready){
+    window.requestAnimationFrame(playGame);
+}}
 
 document.addEventListener("keydown", keyOn);
 document.addEventListener("keyup",keyOff);
 
+let keys = {ArrowUp : false,
+    ArrowDown : false,
+    ArrowLeft : false,
+    ArrowRight : false}
 
 function keyOn(event) {
     event.preventDefault();
-    console.log(event.key);
+    keys[event.key] = true;
+    console.log(keys);
 }
 
 function keyOff(event) {
     event.preventDefault();
-    console.log(event.key);
+    keys[event.key] = false;
+    console.log(keys);
 }
+
